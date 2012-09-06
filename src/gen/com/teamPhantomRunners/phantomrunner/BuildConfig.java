dd92fb0f8bea7421f34e3a998c3c67a3a7bdbd98
Michael Murphy <michael@murphymichael.net>
2012-09-06 15:13:06 -0400

    Initial Loading of blank Android App

---
+++ src/gen/com/teamPhantomRunners/phantomrunner/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.teamPhantomRunners.phantomrunner;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
