44e7220c71c8202e7c5dcc16c2e9958fe26ec815
Michael Murphy <michael@murphymichael.net>
2012-11-07 11:04:24 -0500

    Update src/src/com/teamPhantomRunners/phantomrunner/Run.java
    Updated Run class back to a better final copy. (If you need test data just fill in a Run object where you need it)

--- src/src/com/teamPhantomRunners/phantomrunner/Run.java
+++ src/src/com/teamPhantomRunners/phantomrunner/Run.java
@@ -1,18 +1,79 @@
-package com.teamPhantomRunners.phantomrunner;
-
-public class Run {
+public class Run 
+{
+	private int average_speed;
+	private String speed_units;
+	private double distance;
+	private String distance_units;
+	private int time_hours;
+	private int time_min;
+	private int time_sec;
+	private int calories;
+	private int top_speed;
+	private String Date;
 	
-	public int getDate(){
-		int date = 0;
-		return date;
+	
+	public int getAverage_speed() {
+		return average_speed;
 	}
-	public double getAverage_speed(){return 0.0;}
-	public double getSpeed_units(){return 0.0;}
-	public double getDistance_units(){return 0.0;}
-	public int getTime_hours(){return 0;}
-	public int getTime_min(){return 0;}
-	public int getTime_sec(){return 0;}
-	public int getCalories(){return 0;}
-	public double getTop_speed(){return 0;}
+	public void setAverage_speed(int average_speed) {
+		this.average_speed = average_speed;
+	}
+	public String getSpeed_units() {
+		return speed_units;
+	}
+	public void setSpeed_units(String speed_units) {
+		this.speed_units = speed_units;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	public String getDistance_units() {
+		return distance_units;
+	}
+	public void setDistance_units(String distance_units) {
+		this.distance_units = distance_units;
+	}
+	public int getTime_hours() {
+		return time_hours;
+	}
+	public void setTime_hours(int time_hours) {
+		this.time_hours = time_hours;
+	}
+	public int getTime_min() {
+		return time_min;
+	}
+	public void setTime_min(int time_min) {
+		this.time_min = time_min;
+	}
+	public int getTime_sec() {
+		return time_sec;
+	}
+	public void setTime_sec(int time_sec) {
+		this.time_sec = time_sec;
+	}
+	public int getCalories() {
+		return calories;
+	}
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+	public int getTop_speed() {
+		return top_speed;
+	}
+	public void setTop_speed(int top_speed) {
+		this.top_speed = top_speed;
+	}
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
 
+	
+	
+	
 }
