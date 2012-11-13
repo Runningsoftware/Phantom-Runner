da26e91098c2b479073523649661f694eb46412d
Andrew <andrew.j.vandemark@gmail.com>
2012-11-13 16:04:14 -0500

    Map Overlay class

---
+++ src/src/com/teamPhantomRunners/phantomrunner/MapOverlayItems.java
@@ -0,0 +1,28 @@
+package com.teamPhantomRunners.phantomrunner;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MapOverlayItems extends ItemizedOverlay {
+
+	private ArrayList<OverlayItem>
+	public MapOverlayItems(Drawable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected OverlayItem createItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
