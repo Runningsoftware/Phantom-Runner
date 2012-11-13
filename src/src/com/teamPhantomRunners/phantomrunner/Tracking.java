8881198071cbb5b852e9b1ffa65681e062c40c12
Andrew <andrew.j.vandemark@gmail.com>
2012-11-13 13:03:51 -0500

    Updated Tracking Class

--- src/src/com/teamPhantomRunners/phantomrunner/Tracking.java
+++ src/src/com/teamPhantomRunners/phantomrunner/Tracking.java
@@ -4,13 +4,22 @@ public class Tracking {
 	private long currentLong;
 	private long currentLat;
 	private Route currentRoute;
-	private LiveMetrics metrics;
+	
 	
 	public Tracking()
 	{
-		//code to initialize gps
 		currentRoute = new Route();
-		metrics = new LiveMetrics();
 	}
+	
+	public void updateRoute()
+	{
+		currentRoute.updateRoute();
+	}
+	
+	public String getCurrentLocation()
+	{
+		return currentLat + "," + currentLong;
+	}
+	
 
 }
