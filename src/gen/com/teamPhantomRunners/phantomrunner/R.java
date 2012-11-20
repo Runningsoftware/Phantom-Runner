ef62e62fb86fa218599e97f9599f736f0e1f0fc7
Andrew <andrew.j.vandemark@gmail.com>
2012-11-20 18:08:53 -0500

    Remove Bckgrnd

--- src/gen/com/teamPhantomRunners/phantomrunner/R.java
+++ src/gen/com/teamPhantomRunners/phantomrunner/R.java
@@ -12,20 +12,35 @@ public final class R {
     }
     public static final class drawable {
         public static final int androidmarker=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int ic_launcher2=0x7f020003;
+        public static final int challenge_button=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_launcher2=0x7f020004;
+        public static final int log_button=0x7f020005;
+        public static final int more_button=0x7f020006;
+        public static final int run_button=0x7f020007;
+        public static final int runner_dot_blue=0x7f020008;
+        public static final int set_button=0x7f020009;
+        public static final int timer_button=0x7f02000a;
+        public static final int unlock_button=0x7f02000b;
     }
     public static final class id {
         public static final int editText1=0x7f070000;
-        public static final int mapview=0x7f070001;
-        public static final int menu_challenge=0x7f070004;
-        public static final int menu_instructions=0x7f070006;
-        public static final int menu_logs=0x7f070005;
-        public static final int menu_run=0x7f070002;
-        public static final int menu_settings=0x7f070008;
-        public static final int menu_timer=0x7f070003;
-        public static final int menu_unlocks=0x7f070007;
+        public static final int imageButton1=0x7f070001;
+        public static final int imageButton2=0x7f070002;
+        public static final int imageButton3=0x7f070003;
+        public static final int imageButton4=0x7f070004;
+        public static final int imageButton5=0x7f070005;
+        public static final int imageButton6=0x7f070006;
+        public static final int imageButton7=0x7f070007;
+        public static final int mapview=0x7f070008;
+        public static final int menu_challenge=0x7f07000b;
+        public static final int menu_instructions=0x7f07000d;
+        public static final int menu_logs=0x7f07000c;
+        public static final int menu_run=0x7f070009;
+        public static final int menu_settings=0x7f07000f;
+        public static final int menu_timer=0x7f07000a;
+        public static final int menu_unlocks=0x7f07000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
