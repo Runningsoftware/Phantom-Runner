73dcb8c82a1ad79e1ff77bd9d5c6032a3f7b7883
Michael Murphy <michael@murphymichael.net>
2012-11-20 18:47:28 -0500

    Update src/src/com/teamPhantomRunners/phantomrunner/Utilities.java
    I added a few new functions in this class. One to convert the time from a string into an array of ints. Another that takes a time and converts it to seconds. Another to convert KM to miles.

--- src/src/com/teamPhantomRunners/phantomrunner/Utilities.java
+++ src/src/com/teamPhantomRunners/phantomrunner/Utilities.java
@@ -39,5 +39,37 @@ public class Utilities
 		return null;
 	}
    }
+   
+   public static double kmToMiles(double kilometers)
+   {
+	   return kilometers * 0.621371;
+   }
+   
+   public static int timeInSeconds(int[] times)
+   {
+	   //3600 seconds in an hour 60 in a second.
+	   return ((3600*times[0]) + (60*times[1]) + times[3]);
+   }
+   
+   public static int[] parseTime(String time)
+   {
+	   int[] hrs_min_sec = new int[3];
+	    
+	   String[] s = time.split(":");
+	   
+	   try
+	   {
+	   
+	       hrs_min_sec[0] = Integer.parseInt(s[0]);
+	       hrs_min_sec[1] = Integer.parseInt(s[1]);
+	       hrs_min_sec[2] = Integer.parseInt(s[2]);
+	   
+	       return hrs_min_sec;
+	       
+	   }catch(NumberFormatException e)
+	   {
+		   return null;
+	   }
+   }
 	
 }
